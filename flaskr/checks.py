87a8a77ea9a88f9214d4d2e6f6ce1f081a123146
Chris Dare <cdare77@vt.edu>
2018-07-13 13:57:14 -0400

    updating README.md

--- flaskr/checks.py
+++ flaskr/checks.py
@@ -1,6 +1,6 @@
 import functools
 
-import os
+import os, time
 
 from flask import (
     send_from_directory, current_app, Blueprint, flash, g, redirect, render_template, request, session, url_for
