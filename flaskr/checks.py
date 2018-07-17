26b4506c4b3481fe30a3136d36a5dd3063738175
Chris Dare <cdare77@vt.edu>
2018-07-17 14:54:46 -0400

    Wrote some documentation for checks.py

--- flaskr/checks.py
+++ flaskr/checks.py
@@ -1,6 +1,4 @@
-import functools
-
-import os, time
+import functools, os, time, oval
 
 from multiprocessing import Pool
 
@@ -9,10 +7,12 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
-import oval
+
+########################################################
+#               GLOBAL VARIABLES                       #
+########################################################
 
 bp = Blueprint('checks', __name__, url_prefix='/checks')
-
 
 # Global variable -- Attempting to Serialize an OVAL request into a
 # JSON object can mess with the regex patterns or paths. Thus,
@@ -24,11 +24,24 @@ _requests = []
 # iterable errors
 _drivers = []
 
+# Since the overhead of starting up a pool is large, we want
+# to only instantiate one
 _pool = None
+
+
+
+########################################################
+#                    WEBPAGES                          #
+########################################################
 
 @bp.route('/description', methods=('GET', 'POST'))
 def description():
+    """ Method which renders our description webpage and adds functionality to
+        POST calls. For GET requests, this method essentially creates several
+        OVALRequests. For POST requests, this method converts the OVALRequests
+        into OVALDrivers, which are used in the following webpage """
 
+    # Captures the global requests variable
     global _requests
 
     if request.method == 'POST':
@@ -39,21 +52,27 @@ def description():
         current_app.logger.info(time.ctime() + "\tOVAL drivers initialized")
         
         # we have handled the requests so we no longer need them
-        remove_persist_storage('filenames')
-        remove_persist_storage('processType')
-        remove_persist_storage('coreFactor')
+        _remove_persist_storage('filenames')
+        _remove_persist_storage('processType')
+        _remove_persist_storage('coreFactor')
         del _requests[:] 
         
         return redirect(url_for('checks.results_overview'))
 
-
-    create_descriptions()
+    # Calls all the backend code
+    _create_descriptions()
     return render_template('checks/description.html', requests=_requests)
 
 
 
 @bp.route('/results_overview', methods=('GET', 'POST'))
 def results_overview():
+    """ Method which renders our results webpage and adds functionality
+        to the POST calls. For GET requests, all the drivers execute their
+        tests and pass OVALResults to the HTML. For POST requests, all data
+        is cleared and we return to the upload screen """
+
+
     if request.method == "POST":
         # clean out all unwanted storage
         if _pool:
@@ -62,6 +81,7 @@ def results_overview():
         
         return redirect(url_for('auth.upload'))
 
+    # Captures the global drivers variable
     global _drivers
 
     processType = g.processType
@@ -69,22 +89,33 @@ def results_overview():
 
     if processType == 'parallel':
         current_app.logger.info(time.ctime() + "\tProcess pool with %s processes initialized for execution" % coreFactor)
-        results = _pool.map(get_result, _drivers)
+        results = _pool.map(_get_result, _drivers)
     else:
-        results = [get_result(driver) for driver in _drivers]
+        results = [_get_result(driver) for driver in _drivers]
 
     return render_template('checks/results_overview.html', results=results)
 
 
 
-def get_result(driver):
+########################################################
+#                    HELPER METHODS                    #
+########################################################
+
+
+def _get_result(driver):
+    """ Wrapper function for multiprocessing.Pool.map() function. Creates
+        an OVALResult for each test"""
+    
     result = oval.OVALResult(driver.request.title, driver.execute_tests())
+    current_app.logger.info(time.ctime() + "\tOVAL Result created for %s" % result.title)
+
     return result
 
 
-def get_description(filename):
-        
-    # create a parser and request for each file
+def _get_description(filename):
+    """ Wrapper function for multiprocessing.Pool.map() function. Creates a
+        OVALParser and OVALRequest for each file """
+    
     parser = oval.OVALParser()
     current_app.logger.info(time.ctime() + "\tOVAL Parser for %s initialized" % filename)
     try:
@@ -102,17 +133,20 @@ def get_description(filename):
 
 
 
-def remove_persist_storage(name):
+def _remove_persist_storage(name):
+    """ Helper function to clear server of cookies tied to %name """
     g.pop(name, None)
     session.pop(name, None)
 
 
-def create_descriptions():
+def _create_descriptions():
+    """ Main functionality of checks/description webpage """
 
+    # capture global variables requests and poo
     global _requests
     global _pool
 
-    # capture input from form
+    # extract cookies
     filenames = g.filenames
     processType = g.processType
     coreFactor = g.coreFactor
@@ -120,16 +154,21 @@ def create_descriptions():
     # ensure no requests persisted from last time
     del _requests[:]
 
+    # determine how program is going to be run
     if processType == 'parallel':
         _pool = Pool(processes=int(coreFactor))
         current_app.logger.info(time.ctime() + "\tProcess pool with %s processes initialized for descriptions" % coreFactor)
-        _requests = _pool.map(get_description, filenames)
+        _requests = _pool.map(_get_description, filenames)
     
     else:
         for filename in filenames:
-            _requests.append(get_description(filename))
+            _requests.append(_get_description(filename))
 
 
+
+########################################################
+#                    COOKIE METHODS                    #
+########################################################
 
 @bp.before_app_request
 def load_process_type():
